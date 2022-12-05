9248329c0e2b60abfd4bffcd8cd1917415c510ed
Nico Van den Hooff <nicovandenhooff@gmail.com>
2022-12-04 20:06:48 -0800

    add day 3/4 solutions and input

---
+++ solutions/day3.py
@@ -0,0 +1,53 @@
+# --- Day 3: Rucksack Reorganization ---
+# https://adventofcode.com/2022/day/3
+
+import string
+from utils import get_input_path, read_data
+
+LETTERS = string.ascii_lowercase + string.ascii_uppercase
+PRIORITIES = {k: i + 1 for i, k in enumerate(LETTERS)}
+
+
+def solve_part_1(data):
+    total = 0
+
+    for items in data:
+        # common item is the intersection of the two compartments
+        mid = len(items) // 2
+        compartments = set(items[:mid]), set(items[mid:])
+        common_item = compartments[0].intersection(compartments[1])
+        common_item = list(common_item)[0]
+        priority = PRIORITIES[common_item]
+        total += priority
+
+    return total
+
+
+def solve_part_2(data):
+    total = 0
+
+    for i in range(0, len(data), 3):
+        group = data[i : i + 3]
+
+        for i, g in enumerate(group):
+            group[i] = set(g)
+
+        # common item is the intersection of the three groups
+        common_item = group[0].intersection(group[1], group[2])
+        common_item = list(common_item)[0]
+        priority = PRIORITIES[common_item]
+        total += priority
+
+    return total
+
+
+if __name__ == "__main__":
+    day = 3
+    input_path = get_input_path(day)
+    data = read_data(input_path)
+
+    part_1 = solve_part_1(data)
+    part_2 = solve_part_2(data)
+
+    print(f"The sum of the common item priorities for each rucksack is: {part_1}.")
+    print(f"The sum of the common item priorities for each group is: {part_2}.")
---
+++ solutions/day4.py
@@ -0,0 +1,77 @@
+# --- Day 4: Camp Cleanup ---
+# https://adventofcode.com/2022/day/4
+
+from utils import get_input_path, read_data
+
+
+def process_data(data):
+    processed_data = [i.split(",") for i in data]
+
+    for i, pairs in enumerate(processed_data):
+        assignment_pair = []
+
+        for elf in pairs:
+            start, stop = elf.split("-")
+            start, stop = int(start), int(stop)
+            assignment_pair.append([start, stop])
+
+        processed_data[i] = assignment_pair
+
+    return processed_data
+
+
+def fully_contains(r1_start, r1_end, r2_start, r2_end):
+    # range 1 contains range 2
+    if r1_start <= r2_start and r1_end >= r2_end:
+        return 1
+    # range 2 contains range 1
+    elif r2_start <= r1_start and r2_end >= r1_end:
+        return 1
+    else:
+        return 0
+
+
+def overlaps(r1_start, r1_end, r2_start, r2_end):
+    # one common number in the range
+    if len(set([r1_start, r1_end]).intersection(set([r2_start, r2_end]))) > 0:
+        return 1
+    # overlap from range 1 to 2
+    elif r1_start < r2_start and r2_start < r1_end and r1_end < r2_end:
+        return 1
+    # overlap from range 2 to 1
+    elif r2_start < r1_start and r1_start < r2_end and r2_end < r1_end:
+        return 1
+    else:
+        return 0
+
+
+def solve(processed_data):
+    part_1 = 0
+    part_2 = 0
+
+    for i in processed_data:
+        elf1_start, elf1_end = i[0][0], i[0][1]
+        elf2_start, elf2_end = i[1][0], i[1][1]
+
+        one_range_contains_another = fully_contains(
+            elf1_start, elf1_end, elf2_start, elf2_end
+        )
+
+        ranges_overlap = overlaps(elf1_start, elf1_end, elf2_start, elf2_end)
+
+        part_1 += one_range_contains_another
+        part_2 += one_range_contains_another or ranges_overlap
+
+    return part_1, part_2
+
+
+if __name__ == "__main__":
+    day = 4
+    input_path = get_input_path(day)
+    data = read_data(input_path)
+
+    processed_data = process_data(data)
+    part_1, part_2 = solve(processed_data)
+
+    print(f"{part_1} pairs have ranges that fully contain the other.")
+    print(f"{part_2} pairs have ranges that overlap with one another.")
